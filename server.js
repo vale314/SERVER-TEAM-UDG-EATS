077e47e5f185423c8be8027cb4a8a3124b852e1c
Vale314 <electrovale314@hotmail.com>
2020-11-25 01:48:01 -0600

    1.8

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const { connection } = require("./config/db");
 
 const fs = require("fs");
 
-const key = fs.readFileSync("./ssl/key.pem");
-const cert = fs.readFileSync("./ssl/cert.pem");
+// const key = fs.readFileSync("./ssl/key.pem");
+// const cert = fs.readFileSync("./ssl/cert.pem");
 
-const server = require("https").createServer({ key: key, cert: cert }, app);
+const server = require("http").createServer(app);
 
 connection();
 
@@ -19,6 +19,6 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/seller", require("./routes/seller"));
 app.use("/api/users", require("./routes/users"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
